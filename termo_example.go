f4c359435bdf6f76311936e6748df468cb8df9ec
Jon Valdés <juanval@gmail.com>
2014-09-06 18:58:02 +0200

    Updated example with new API

--- termo_example.go
+++ termo_example.go
@@ -15,6 +15,8 @@ func main() {
 	if err := termo.Init(); err != nil {
 		panic(err)
 	}
+	termo.EnableMouseEvents()
+	termo.ShowCursor()
 
 	// Reset terminal if we panic
 	defer func() {
@@ -33,7 +35,7 @@ func main() {
 	f := termo.NewFramebuffer(w, h)
 
 	// Start reading input from stdin
-	keyChan := make(chan termo.ScanCode)
+	keyChan := make(chan termo.ScanCode, 100)
 	go func() {
 		for {
 			s, err := termo.ReadScanCode()
@@ -47,14 +49,31 @@ func main() {
 	posx := 10
 	posy := 10
 	startT := time.Now().UnixNano()
+	var lastScanCode termo.ScanCode
 
 	// Main loop
 	for {
+		// Clear framebuffer
+		f.Clear()
+
+		// Draw outer frame
+		f.ASCIIRect(0, 0, w, h, true, false)
 
 		// Read keyboard
+	readAgain:
 		select {
 		case s := <-keyChan:
-			if s.IsEscapeCode() {
+			lastScanCode = s
+			if s.IsMouseMoveEvent() {
+				x, y := s.MouseCoords()
+				termo.SetCursor(x, y)
+			} else if s.IsMouseDownEvent() {
+				x, y := s.MouseCoords()
+				f.SetRect(x-2, y-2, 5, 5, termo.CellState{termo.AttrBold, termo.ColorYellow, termo.ColorYellow}, '#')
+			} else if s.IsMouseUpEvent() {
+				x, y := s.MouseCoords()
+				f.SetRect(x-2, y-2, 5, 5, termo.CellState{termo.AttrBold, termo.ColorGreen, termo.ColorGreen}, '#')
+			} else if s.IsEscapeCode() {
 				switch s.EscapeCode() {
 				case 65: // Up
 					posy--
@@ -65,26 +84,24 @@ func main() {
 				case 68: // Left
 					posx--
 				default:
-					termo.Stop()
-					fmt.Println(s.EscapeCode())
-					return
+					//termo.Stop()
+					//termo.PutText(fmt.Sprint(s.EscapeCode()))
+					//return
 				}
-			}
-			if !s.IsEscapeCode() {
+			} else {
 				r := s.Rune()
 				// Exit if Ctrl+C or Esc are pressed
 				if r == 3 || r == 27 {
+					termo.Stop()
 					os.Exit(0)
 				}
 			}
+			goto readAgain
 		default:
 		}
 
-		// Clear framebuffer
-		f.Clear()
-
 		// Draw the rectangle
-		f.PutRect(posx, posy, 20, 20, termo.CellState{termo.AttrBold, termo.ColorYellow, termo.ColorRed}, '2')
+		f.SetRect(posx, posy, 20, 20, termo.CellState{termo.AttrBold, termo.ColorYellow, termo.ColorRed}, '2')
 
 		// Draw the sine wave
 		t := float64(time.Now().UnixNano()-startT) / 500000.0
@@ -92,22 +109,18 @@ func main() {
 		chars := []rune{'.', 'o', '*', 'o', '.'}
 		s := termo.CellState{termo.AttrNone, termo.ColorGreen, termo.ColorDefault}
 
-		for i := 0; i < w; i++ {
+		for i := 1; i < w-1; i++ {
 			sh := 6 + int(5*math.Sin(0.001*t+float64(i)/float64(w)*math.Pi*2))
-			for j := 0; j < 5; j++ {
-				f.Put(i, sh-2+j, s, chars[j])
+			for j := 1; j < 5; j++ {
+				f.Set(i, sh-2+j, s, chars[j])
 			}
 		}
 
 		// Draw text
-		f.PutText(4, h-4, termo.CellState{termo.AttrNone, termo.ColorRed, termo.ColorGray}, "Press Up/Down/Left/Right to move")
-		f.PutText(4, h-3, termo.CellState{termo.AttrBold, termo.ColorRed, termo.ColorDefault}, "Ctrl+C or Esc to exit")
-
-		// Draw outer frame
-		f.PutRect(0, 0, w, 1, termo.CellState{termo.AttrNone, termo.ColorDefault, termo.ColorDefault}, '-')
-		f.PutRect(0, 0, 1, h, termo.CellState{termo.AttrNone, termo.ColorDefault, termo.ColorDefault}, '|')
-		f.PutRect(w-1, 0, 1, h, termo.CellState{termo.AttrNone, termo.ColorDefault, termo.ColorDefault}, '|')
-		f.PutRect(0, h-1, w, 1, termo.CellState{termo.AttrNone, termo.ColorDefault, termo.ColorDefault}, '-')
+		f.AttribRect(4, h-5, 10, 10, termo.StateDefault)
+		f.SetText(4, h-5, fmt.Sprint(lastScanCode))
+		f.SetText(4, h-4, "Press Up/Down/Left/Right to move")
+		f.SetText(4, h-3, "Ctrl+C or Esc to exit")
 
 		// Push framebuffer to screen
 		f.Flush()
